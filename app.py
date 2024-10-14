81a4b858af2dee4bad0f34ef23954e8ec85f26d5
Utkarsh Kuchhal <kuchhalutkarsh17@gmail.com>
2024-10-14 20:19:59 +0530

    Vercel File Added

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def fetchAllOrdersAPI():
     return jsonify({"success": True, "data": orders}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001) 
+    app.run(debug=True, port=5001, host='0.0.0.0') 
